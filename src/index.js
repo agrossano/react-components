7eb4eea7f303ec971fd4df547e06d6ac5794341f
agrossano <grossano.alessandro@gmail.com>
2020-07-29 17:55:37 +0200

    Approval cards implementate

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Ale"
-        timeAgo="Today at 5:10PM"
-        comment="Ottimo post!"
-        avatarImg={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jeanne"
-        timeAgo="Today at 2:30AM"
-        comment="Ben scritto!"
-        avatarImg={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Peter"
-        timeAgo="Yesterday at 8:30PM"
-        comment="Grazie per il commento"
-        avatarImg={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Ale"
+          timeAgo="Today at 5:10PM"
+          comment="Ottimo post!"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jeanne"
+          timeAgo="Today at 2:30AM"
+          comment="Ben scritto!"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Peter"
+          timeAgo="Yesterday at 8:30PM"
+          comment="Grazie per il commento"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
